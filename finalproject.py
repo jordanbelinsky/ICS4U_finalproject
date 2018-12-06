f626435ae87337afd6613efce028d8a015daf017
Jordan Belinsky <jordanbelinsky1@gmail.com>
2018-12-06 10:01:34 -0500

    Update finalproject.py
    cleaned up the code a bit

--- finalproject.py
+++ finalproject.py
@@ -1,7 +1,11 @@
-# Day 1
-# Ritchie Dimaria
-# Final project
-# Desmos
+"""
+Names: Ritchie Dimaria, Jeremy Weisberg, Jordan Belinsky, Alex Giannoulis
+Project: Conway's Game of Life
+"""
+
+########################
+# Import and Variables #
+########################
 import pygame
 WHITE = (255,255,255)
 BLACK = (  0,  0,  0)
@@ -9,24 +13,27 @@ GREY = ( 50, 50, 50)
 HEIGHT = 640
 WIDTH = 480
 size = 10
-## OBJECTS
+
+###########
+# Objects #
+###########
 class Cell(object):
-    
     def __init__(self, alive = False, x =0,y=0 ,size = 10):
         self.alive = alive
         self.x = x
         self.y = y
         self.size = size
-
     def checkNeighbors(self,other):
         pass
-
     def draw(self,screen):
         x = self.x
         y = self.y
         size = self.size
         pygame.draw.rect(screen,BLACK,(x,y,size,size),0)
-## FUNCTIONS
+
+#############
+# Functions #
+#############
 def draw_grid():
     for i in range(0,HEIGHT,size):
         pygame.draw.line(game_window,BLACK,(i,0),(i,WIDTH),1)
@@ -36,9 +43,11 @@ def redraw_game_window():
     game_window.fill(GREY)
     draw_grid()
     pygame.display.update()
-## MAIN GAME  
+
+#############
+# Main Game # 
+#############
 pygame.init()
 game_window=pygame.display.set_mode((HEIGHT,WIDTH))
-
 while True:
     redraw_game_window()
