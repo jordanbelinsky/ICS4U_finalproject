e14f99e776a0ec1aa9a54024987eed4b1656b013
nohowow <jeremyweisberg1@gmail.com>
2018-12-11 09:06:25 -0500

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -7,6 +7,7 @@ Project: Conway's Game of Life
 # Import and Variables #
 ########################
 import pygame
+gameOn = False
 WHITE = (255,255,255)
 BLACK = (  0,  0,  0)
 GREY = ( 50, 50, 50)
@@ -18,19 +19,21 @@ size = 10
 # Objects #
 ###########
 class Cell(object):
-    def __init__(self, alive = False, x =0,y=0 ,size = 10):
+    def __init__(self, alive = False, x=0, y=0 ,size = 10):
         self.alive = alive
         self.x = x
         self.y = y
         self.size = size
     def checkNeighbors(self,other):
-        pass
+        pass                
     def draw(self,screen):
         x = self.x
         y = self.y
         size = self.size
         pygame.draw.rect(screen,BLACK,(x,y,size,size),0)
 
+class Grid(object):
+    def __init__(self):
 #############
 # Functions #
 #############
@@ -39,6 +42,7 @@ def draw_grid():
         pygame.draw.line(game_window,BLACK,(i,0),(i,WIDTH),1)
     for i in range(0,WIDTH,size):
         pygame.draw.line(game_window,BLACK,(0,i),(HEIGHT,i),1)
+                                                                
 def redraw_game_window():
     game_window.fill(GREY)
     draw_grid()
@@ -49,5 +53,9 @@ def redraw_game_window():
 #############
 pygame.init()
 game_window=pygame.display.set_mode((HEIGHT,WIDTH))
-while True:
+for event in pygame.event.get():                    #Main Pygame Loop
+    if event.type == pygame.KEYDOWN:                #Check if a key is pressed
+        if event.key == pygame.K_SPACE:             #If space is pressed, turn gameOn, meaning the simulation is on
+            gameOn=True                             #Space is to be pressed after user selects all alive cells
+while gameOn==True:
     redraw_game_window()
