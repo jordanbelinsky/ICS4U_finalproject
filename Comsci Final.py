e9613d198c0a4badaf38574feba50db1c43f06bc
Alex Giannoulis <alex.giannoulis5@gmail.com>
2018-12-17 09:33:12 -0500

    Fix merge
    - next generation button added

--- Comsci Final.py
+++ Comsci Final.py
@@ -7,26 +7,26 @@ rules = '''
     3. Any live cell with more than three live neighbours dies, as if by overcrowding.
     4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
 
+#Initializations:
 
-# Initializations #
-
-####### Constants #######
+#CONSTANTS
+#-------------------
 gridSize = 100
 size = 10
 FPS = 100
 WIDTH,HEIGHT = 1000,1000
 generation = 0
-#########################
+#-------------------
 
-# Define a 2D board (List containing lists) #
+#Define a 2D board (List containing lists)
 board = [[False for i in range(gridSize)] for j in range(gridSize)]
 
-# Set up colors for ease of use #
+#Set up colors for ease of use
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (211, 211, 211)
 
-# Set up pygame #
+#Set up pygame
 pygame.init()
 font = pygame.font.SysFont("Ariel Black",30)
 screen = pygame.display.set_mode((WIDTH,HEIGHT)) # Define the surface for the simulation to run on
@@ -37,16 +37,16 @@ clock = pygame.time.Clock()
 
 text_X = 10                             # initial coordinates
 text_Y = 10
-# Function for returning which row and column the mouse is in #
+#Function for returning which row and column the mouse is in
 def mousePos():
     x, y = pygame.mouse.get_pos()
     return (x//10, y//10)
 
-# Function to find number of live neighbors #
+#Function to find number of live neighbors
 def findNeighbors(row, column):
     alive = 0
 
-    # Horizontally adjacent #
+    #Horizontally adjacent
     if row > 0:
         if board[row-1][column]:
             alive += 1
@@ -60,7 +60,7 @@ def findNeighbors(row, column):
         if board[row][column+1]:
             alive += 1
 
-    # Diagonally adjacent #
+    #Diagonally adjacent
     if row > 0 and column > 0:
         if board[row-1][column-1]:
             alive += 1
@@ -74,14 +74,14 @@ def findNeighbors(row, column):
         if board[row+1][column-1]:
             alive += 1
 
-    # Return the final count (0-8) #
+    #Return the final count (0-8)
     return alive
 
-# Turn a space of the grid on #
+#Turn a space of the grid on
 def giveLife(row, col):
     pygame.draw.rect(screen, BLACK, (row*10,col*10,size,size),0)
 
-# Turn a space of the grid off #
+#Turn a space of the grid off
 def killRuthlessly(row, col):
     pygame.draw.rect(screen, WHITE, (row*10,col*10,size,size),0)
 
@@ -97,7 +97,7 @@ def draw_grid():
 
 def save_grid():
     if run == False:
-        # name=input("Enter the name of the file you want to save")
+        #name=input("Enter the name of the file you want to save")
         file_out = open('map','w')
         for i in range(len(board)):
             for j in range(len(board[i])):
@@ -107,7 +107,7 @@ def save_grid():
 
 def load_grid():
     if run == False:
-        # fname=input("Enter the filename of the map you'd like to open")
+        #fname=input("Enter the filename of the map you'd like to open")
         newlst=[]
         file_in = open('map','r')
         lines = file_in.readlines()
@@ -132,11 +132,11 @@ def place_oscillator():
 def place_stillLife():
     pass
 
-#Main loop
+# Main loop #
 run = False
 while True:
     
-    #Draw the board as rectangles
+    # Draw the board as rectangles #
     for row in range(len(board)):
         for col in range(len(board)):
             if board[row][col]:
@@ -144,7 +144,7 @@ while True:
             if not board[row][col]:
                 killRuthlessly(row, col)
 
-    # Process Events #        
+    #Process Events            
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -161,36 +161,39 @@ while True:
                 newlst = load_grid()
                 board=newlst
                 
-            # Navigate generations #
-            if event.key == pygame.K_RIGHT:
-                '''TODO: Use the right key to navigate forwards 1 generation'''
-                run = True
-                if board == tempboard:
-                    run == False
-            if event.key == pygame.K_LEFT:
-                '''TODO: Use the left key to navigate back 1 generation'''
 
 
-<<<<<<< HEAD
-=======
+
+            if run == False:
+                if event.key == pygame.K_RIGHT:
+                    for x in range(1):
+                        tempboard = [[False for i in range(gridSize)] for j in range(gridSize)]
+                        for row in range(len(board)):
+                            for col in range(len(board)):
+                                neighborcount = findNeighbors(row, col)
+                                if board[row][col]: #any live cell
+                                    if neighborcount < 2:
+                                        tempboard[row][col] = False #dies
+                                    if neighborcount > 3: #With more than three live neighbors
+                                        tempboard[row][col] = False #dies
+                                    if neighborcount == 2 or neighborcount == 3:
+                                        tempboard[row][col] = True #lives on to the next generation 
+                                elif not board[row][col]: #any dead cell
+                                    if neighborcount == 3: #with exactly three live neighbors
+                                        tempboard[row][col] = True #becomes a live cell
+                        board = tempboard
+                        generation += 1
+                        run = False              
+                if event.key == pygame.K_LEFT:
+                    '''TODO: Use the left key to navigate back 1 generation'''
+
+
+
+
+
+
 
                 
-            if event.key == pygame.K_RIGHT:
-                '''TODO: Use the right key to navigate forwards 1 generation'''
-                run = True
-                if board == tempboard:
-                    run == False
-            if event.key == pygame.K_LEFT:
-                '''TODO: Use the left key to navigate back 1 generation'''
-
-
-
-
-
-
-
-                
->>>>>>> parent of 9b4cf3c... Add next gen function
             if event.key == pygame.K_c:
                 if run == True:
                     run = not run
@@ -205,7 +208,7 @@ while True:
                         board[row][col] = random.choice(possibilities)
 
 
-    # Rules #
+    #RULES
     if run:
         tempboard = [[False for i in range(gridSize)] for j in range(gridSize)]
         for row in range(len(board)):
@@ -224,6 +227,7 @@ while True:
         board = tempboard
         generation += 1
         
+
     click = pygame.mouse.get_pressed()
     if click[0]:
         mouseX,mouseY = mousePos()
@@ -235,6 +239,7 @@ while True:
         board[mouseX][mouseY] = False
         if not run:
             generation = 0
+
         
     draw_grid()
     message=('Generation: '+str(generation))
