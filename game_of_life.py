f0f197ae2e5b85b0578a8fc2d24d6c30a412c14f
Jordan Belinsky <jordanbelinsky@yahoo.ca>
2019-01-08 10:23:24 -0500

    Organization
    - gave headers for each section
    - organized comment spacing

--- game_of_life.py
+++ game_of_life.py
@@ -1,35 +1,34 @@
+# Import #
 import pygame
 import random
 
+# Rules List #
 rules = '''
-    1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-    2. Any live cell with two or three live neighbours lives on to the next generation.
-    3. Any live cell with more than three live neighbours dies, as if by overcrowding.
-    4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
+        1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        2. Any live cell with two or three live neighbours lives on to the next generation.
+        3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+        4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        '''
 
-#Initializations:
-
-#CONSTANTS
-#-------------------
+# Constants #
 gridSize = 100
 size = 10
 FPS = 100
 WIDTH,HEIGHT = 1000,1000
 generation = 0
 count = 0
-#-------------------
 
-#Define a 2D board (List containing lists)
+# 2D Board #
 board = [[False for i in range(gridSize)] for j in range(gridSize)]
 
-#Set up colors for ease of use
+# Colors #
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (211, 211, 211)
 coolGrey = (130,130,130)
 RED = (255,0,0)
 
-#Set up pygame
+# Pygame Setup #
 pygame.init()
 font = pygame.font.SysFont("Ariel Black",30)
 screen = pygame.display.set_mode((WIDTH,HEIGHT)) # Define the surface for the simulation to run on
@@ -39,25 +38,26 @@ screen.fill(WHITE) # Fill the screen white
 pygame.display.update()
 clock = pygame.time.Clock()
 
-initialCoord = None # Coordinates for drag&drop
+# Coordinates #
+initialCoord = None
 timer=0
-
-text_X = 10                             # initial coordinates
+text_X = 10                     
 text_Y = 10
 
+# Preset Menu #
 extendo = False # Pullout menu starts closed
 smallGlider = False # Drag/Dropping smallGlider
-#Function for returning which row and column the mouse is in
 
+# Mouse Position #
 def mousePos():
     x, y = pygame.mouse.get_pos()
     return (x//10, y//10)
 
-#Function to find number of live neighbors
+# Find Live Neighbours #
 def findNeighbors(row, column):
     alive = 0
 
-    #Horizontally adjacent
+    # Horizontally adjacent #
     if row > 0:
         if board[row-1][column]:
             alive += 1
@@ -71,7 +71,7 @@ def findNeighbors(row, column):
         if board[row][column+1]:
             alive += 1
 
-    #Diagonally adjacent
+    # Diagonally adjacent #
     if row > 0 and column > 0:
         if board[row-1][column-1]:
             alive += 1
@@ -85,20 +85,22 @@ def findNeighbors(row, column):
         if board[row+1][column-1]:
             alive += 1
 
-    #Return the final count (0-8)
+    # Return the final count (0-8)
     return alive
 
-#Turn a space of the grid on
+# Activate a Grid Space #
 def giveLife(row, col):
     board[row][col] = True
 
-#Turn a space of the grid off
+# Deactivate a Grid Space #
 def killRuthlessly(row, col):
     board[row][col] = False
 
+# Draw Temporary Rectangle #
 def drawTempRect(row,col):
     pygame.draw.rect(screen,coolGrey, (row*10,col*10,size,size),0)
 
+# Shape Preset - Small Glider #
 def drawSmallGlider(row,col):
     global smallGlider
     drawTempRect(row,col)
@@ -114,7 +116,8 @@ def drawSmallGlider(row,col):
         giveLife(row-1,col-2)
     if click[2]:
         smallGlider = False
-        
+
+# Setup GUI #
 def checkGui(mouseX,mouseY):
     global extendo,smallGlider
     if (mouseX > 97 and mouseY>40 and mouseY<49):
@@ -128,6 +131,7 @@ def checkGui(mouseX,mouseY):
         if mouseX>77 and mouseX<80 and mouseY>40 and mouseY<49:
             extendo=False
         
+# Display GUI #
 def drawGui():
     global extendo
     if extendo == True:
@@ -150,16 +154,19 @@ def drawGui():
         pygame.draw.polygon(screen,WHITE,[(995,439),(995,449),(985,444)],0)
         pygame.draw.polygon(screen,WHITE,[(995,459),(995,469),(985,464)],0)
 
+# Display Text #
 def drawText(message):
     text = font.render(message, 1, BLACK) # put the font and the message together
     screen.blit(text,(text_X,text_Y))
 
+# Display Grid #
 def draw_grid():
     for i in range(0,WIDTH,size):
         pygame.draw.line(screen,GREY,(i,0),(i,HEIGHT),1)
     for i in range(0,HEIGHT,size):
         pygame.draw.line(screen,GREY,(0,i),(WIDTH,i),1)
 
+# Save Grid File #
 def save_grid():
     if run == False:
         #name=input("Enter the name of the file you want to save")
@@ -170,6 +177,7 @@ def save_grid():
             file_out.write('\n')
         file_out.close()
 
+# Load Grid File #
 def load_grid():
     if run == False:
         #fname=input("Enter the filename of the map you'd like to open")
@@ -182,11 +190,13 @@ def load_grid():
         file_in.close()
         return newlst
 
+# Enable Drag and Drop #
 def dragdrop(inital,x,y):
     startx,starty = inital
     print(str(startx)+' '+str(starty)+' '+str(x)+' '+str(y))
     pygame.draw.polygon(screen,RED,((startx*10, starty*10), (startx*10,y*10),(x*10,y*10),(x*10,starty*10)),3)
-   
+
+# Save Shape File #
 def save_shape(startx,starty,x,y):
     if run == False:
         #name=input("Enter the name of the file you want to save")
@@ -198,6 +208,7 @@ def save_shape(startx,starty,x,y):
             file_out.write('\n')
         file_out.close()
 
+# Load Shape File #
 def load_shape():
     if run == False:
         #fname=input("Enter the filename of the map you'd like to open")
@@ -210,30 +221,33 @@ def load_shape():
         file_in.close()
         return newlst
 
+# Place Spaceship #
 def place_spaceship():
     pass
 
+# Place Oscillator #
 def place_oscillator():
     pass
 
+# Place StillLife #
 def place_stillLife():
     pass
 
-#Main loop
+# Main Loop #
 genCount = 0
 mouse_mode=True
 run = False
 
 while True:
     mouseX,mouseY = mousePos()
-    #Draw the board as rectangles
+    # Draw the board as rectangles #
     for row in range(len(board)):
         for col in range(len(board)):
             if board[row][col]:
                 pygame.draw.rect(screen, BLACK, (row*10,col*10,size,size),0)
             if not board[row][col]:
                 pygame.draw.rect(screen, WHITE, (row*10,col*10,size,size),0)
-    #Process Events            
+    # Process Events #    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -242,20 +256,22 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 break
+            # Run #
             if event.key == pygame.K_RETURN:
                 run = not run
+            # Save #
             if event.key == pygame.K_s:
                 save_grid()
+            # Load Grid #
             if event.key == pygame.K_l:
                 newlst = load_grid()
                 board=newlst
             if event.key == pygame.K_f:
                 mouse_mode = not mouse_mode
-
+            # Load Shape #
             if event.key == pygame.K_k:
                 newlst=load_shape()
-
-                
+            # Next Generation #
             if event.key == pygame.K_RIGHT:
                 for x in range(1):
                         tempboard = [[False for i in range(gridSize)] for j in range(gridSize)]
@@ -274,15 +290,14 @@ while True:
                                         tempboard[row][col] = True #becomes a live cell
                         board = tempboard
                         generation += 1
-                        run = False       
-                
-
+                        run = False
+            # Clear Screen #
             if event.key == pygame.K_c:
                 if run == True:
                     run = not run
                 generation = 0
                 board = [[False for i in range(gridSize)] for j in range(gridSize)]
-
+            # Random #
             if event.key == pygame.K_r:
                 generation = 0
                 possibilities = [False, False, True]
@@ -291,7 +306,7 @@ while True:
                         board[row][col] = random.choice(possibilities)
 
 
-    #RULES
+    # Rules #
     if run:
         tempboard = [[False for i in range(gridSize)] for j in range(gridSize)]
         for row in range(len(board)):
@@ -311,6 +326,7 @@ while True:
         generation += 1
         genCount = 1
 
+    # User Input (Drawing) #
     click = pygame.mouse.get_pressed()
     if mouse_mode:
         if click[0]:
@@ -336,9 +352,7 @@ while True:
                 save_shape(startx,starty,mouseX,mouseY)
             initialCoord = None
 
-
-
-            
+    # Redraw Screen #
     draw_grid()
     drawGui()
     if smallGlider:
