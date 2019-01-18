fe26913a1c49ce622efeaa021cc88fa52deb1e04
Jordan Belinsky <jordanbelinsky@yahoo.ca>
2019-01-18 11:08:10 -0500

    Merge Ritchie and Jordan
    - added rules page(s)
    - added header font
    - cleaned up extendo secondary UI
    - fixed click recognition

--- game_of_life.py
+++ game_of_life.py
@@ -1,12 +1,13 @@
 import pygame
 import random
 import pygame.midi
-import game_of_life_class
+
 rules = '''
-    1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-    2. Any live cell with two or three live neighbours lives on to the next generation.
-    3. Any live cell with more than three live neighbours dies, as if by overcrowding.
-    4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
+        1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        2. Any live cell with two or three live neighbours lives on to the next generation.
+        3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+        4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        '''
 
 #Initializations:
 
@@ -30,6 +31,14 @@ GREY = (211, 211, 211)
 coolGrey = (130,130,130)
 RED = (255,0,0)
 
+
+# COPY #
+#states
+rules = False
+rules_2 = False
+main_game = True
+########
+
 #Set up pygame
 pygame.init()
 pygame.midi.init()
@@ -38,17 +47,20 @@ pygame.midi.init()
 player = pygame.midi.Output(0)
 player.set_instrument(120)
 
+# COPY #
 font = pygame.font.SysFont("Ariel Black",30)
+header = pygame.font.SysFont("Ariel Black",38) 
 screen = pygame.display.set_mode((WIDTH,HEIGHT)) # Define the surface for the simulation to run on
 pygame.display.set_caption('Conway\'s Game of Life')
 smallGlide = font.render('Small glider', False, (0, 0, 0))
+rules_text = font.render('Rules', False, (0, 0, 0))
 screen.fill(WHITE) # Fill the screen white
 pygame.display.update()
 clock = pygame.time.Clock()
+########
 
 initialCoord = None # Coordinates for drag&drop
 timer=0
-
 text_X = 10                             # initial coordinates
 text_Y = 10
 
@@ -60,12 +72,6 @@ smallGlider = False # Drag/Dropping smallGlider
 smallgliderlist= [[False,True,False],
                       [False,False,True],
                       [True,True,True]]
-
-
-
-
-
-
 
 
 def mousePos():
@@ -141,44 +147,111 @@ def drawSmallGlider(row,col):
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
                 smallgliderlist = rotateShape(smallgliderlist)
+
+# COPY #
 def checkGui(mouseX,mouseY):
-    global extendo,smallGlider
+    global extendo,smallGlider,rules,main_game
     if (mouseX > 97 and mouseY>40 and mouseY<49):
         extendo = True
     if extendo == True:
-        if (mouseX>82 and mouseX<98 and mouseY > 2 and mouseY <18):
+        if (mouseX > 82 and mouseX < 98 and mouseY > 2 and mouseY < 10):
             smallGlider = True
-            
-        if mouseX>77:
+        if (mouseX > 82 and mouseX < 98 and mouseY > 69 and mouseY < 77):
+            rules = True
+            run = False
+        if mouseX > 77:
             killRuthlessly(mouseX,mouseY)
-        if mouseX>77 and mouseX<80 and mouseY>40 and mouseY<49:
-            extendo=False
-        
-def drawGui():
+        if mouseX > 77 and mouseX < 80 and mouseY > 40 and mouseY < 49:
+            extendo = False
+########
+
+# COPY #
+def drawGui():# COPY #
     global extendo
     if extendo == True:
         pygame.draw.rect(screen,coolGrey,(800,0,200,HEIGHT),0)
         pygame.draw.rect(screen,BLACK,(800,1,199,HEIGHT-2),3)
-        pygame.draw.rect(screen,WHITE,(820,20,160,160),0)
-        pygame.draw.rect(screen,WHITE,(820,200,160,160),0)
-        pygame.draw.rect(screen,WHITE,(820,380,160,160),0)
-        pygame.draw.rect(screen,WHITE,(820,560,160,160),0)
+        pygame.draw.rect(screen,WHITE,(820,50,160,80),0)    # small glider
+        pygame.draw.rect(screen,WHITE,(820,165,160,80),0)   # shape 2
+        pygame.draw.rect(screen,WHITE,(820,280,160,80),0)   # shape 3
+        pygame.draw.rect(screen,WHITE,(820,395,160,80),0)   # shape 4
+        pygame.draw.rect(screen,WHITE,(820,690,160,80),0)   # rules
         pygame.draw.rect(screen,BLACK,(777,402,24,86),0)
         pygame.draw.rect(screen,coolGrey,(780,405,19,80),0)
         pygame.draw.polygon(screen,WHITE,[(785,419),(785,429),(795,424)],0)
         pygame.draw.polygon(screen,WHITE,[(785,439),(785,449),(795,444)],0)
         pygame.draw.polygon(screen,WHITE,[(785,459),(785,469),(795,464)],0)
-        screen.blit(smallGlide,(820,25))
+        screen.blit(smallGlide,(820,25))    # small glider
+        screen.blit(smallGlide,(820,140))   # shape 2
+        screen.blit(smallGlide,(820,255))   # shape 3
+        screen.blit(smallGlide,(820,370))   # shape 4
+        screen.blit(rules_text,(870,720))
     else:
         pygame.draw.rect(screen,BLACK,(980,405,50,80),3)
         pygame.draw.rect(screen,coolGrey,(980,405,50,80),0)
         pygame.draw.polygon(screen,WHITE,[(995,419),(995,429),(985,424)],0)
         pygame.draw.polygon(screen,WHITE,[(995,439),(995,449),(985,444)],0)
         pygame.draw.polygon(screen,WHITE,[(995,459),(995,469),(985,464)],0)
+########
 
 def drawText(message):
     text = font.render(message, 1, BLACK) # put the font and the message together
     screen.blit(text,(text_X,text_Y))
+
+# COPY #
+# Draw Rules #
+def draw_rules_page_one():
+    title = header.render("Conway's Game Of Life", 1, BLACK)
+    description_1 = font.render('This is a game where you draw set patterns of squares on the screen,', 1, BLACK)
+    description_2 = font.render('which are then animated and react to the rules below, forming a living ecosystem.', 1, BLACK) 
+    rule_1 = font.render('1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.', 1, BLACK)
+    rule_2 = font.render('2. Any live cell with two or three live neighbours lives on to the next generation.', 1, BLACK)
+    rule_3 = font.render('3. Any live cell with more than three live neighbours dies, as if by overcrowding.', 1, BLACK)
+    rule_4 = font.render('4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', 1, BLACK)
+    names = font.render('By: Jordan Belinsky, Ritchie DiMaria, Alex Giannoulis, and Jeremy Weisberg', 1, BLACK)
+    escape =  font.render('Press E to return to the main game', 1, BLACK)
+    screen.blit(title, (350, 50))
+    screen.blit(description_1, (150, 125))
+    screen.blit(description_2, (95, 175))
+    screen.blit(rule_1, (50, 275))
+    screen.blit(rule_2, (100, 340))
+    screen.blit(rule_3, (100, 415))
+    screen.blit(rule_4, (50, 490))
+    screen.blit(names, (105, 585))
+    screen.blit(escape, (335, 700))
+    pygame.draw.rect(screen, BLACK, (380,750,100,30), 0)
+    pygame.draw.rect(screen, coolGrey, (530,750,100,30), 0)
+
+def draw_rules_page_two():
+    title = header.render("Conway's Game Of Life", 1, BLACK)
+    description_1 = font.render('This is a game where you draw set patterns of squares on the screen,', 1, BLACK)
+    description_2 = font.render('which are then animated and react to the rules below, forming a living ecosystem.', 1, BLACK)
+    key_title = font.render('Keyboard Shortcuts:', 1, BLACK)
+    key_enter = font.render('Enter: Start/Pause Game', 1, BLACK)
+    key_s = font.render('S: Save Map', 1, BLACK)
+    key_l = font.render('L: Load Map', 1, BLACK)
+    key_f = font.render('F: Enable/Disable Drag Select', 1, BLACK)
+    key_k = font.render('K: Load Shape', 1, BLACK)
+    key_r = font.render('R: Random Map', 1, BLACK)
+    key_c = font.render('C: Clear Map', 1, BLACK)
+    key_right = font.render('Right Arrow: Fast Forward', 1, BLACK)
+    escape =  font.render('Press E to return to the main game', 1, BLACK)
+    screen.blit(title, (350, 50))
+    screen.blit(description_1, (150, 125))
+    screen.blit(description_2, (95, 175))
+    screen.blit(key_title, (400, 275))
+    screen.blit(key_enter, (380, 320))
+    screen.blit(key_s, (440, 355))
+    screen.blit(key_l, (440, 390))
+    screen.blit(key_f, (355, 425))
+    screen.blit(key_k, (435, 460))
+    screen.blit(key_r, (435, 495))
+    screen.blit(key_c, (440, 530))
+    screen.blit(key_right, (375, 565))
+    screen.blit(escape, (335, 700))
+    pygame.draw.rect(screen, coolGrey, (380,750,100,30), 0)
+    pygame.draw.rect(screen, BLACK, (530,750,100,30), 0)
+########
 
 def draw_grid():
     for i in range(0,WIDTH,size):
@@ -245,14 +318,11 @@ def place_stillLife():
     pass
 
 
-
-
-
 #Main loop
 genCount = 0
-mouse_mode=True
+mouse_mode = True
 run = False
-while True:
+while main_game == True:
     mouseX,mouseY = mousePos()
     #Draw the board as rectangles
     for row in range(len(board)):
@@ -279,9 +349,24 @@ while True:
                 board=newlst
             if event.key == pygame.K_f:
                 mouse_mode = not mouse_mode
-
             if event.key == pygame.K_k:
                 newlst=load_shape()
+            # COPY #
+            if rules:
+                if event.key == pygame.K_RIGHT:
+                    rules = False
+                    rules_2 = True
+                if event.key == pygame.K_e:
+                    rules = False
+                    run = False
+            if rules_2:
+                if event.key == pygame.K_LEFT:
+                    rules = True
+                    rules_2 = False
+                if event.key == pygame.K_e:
+                    rules_2 = False
+                    run = False
+            ########
 
                 
             if event.key == pygame.K_RIGHT:
@@ -314,13 +399,11 @@ while True:
                 board = [[False for i in range(gridSize)] for j in range(gridSize)]
 
             if event.key == pygame.K_r:
-                if smallGlider == False:
                 generation = 0
                 possibilities = [False, False, True]
                 for row in range(gridSize):
                     for col in range(gridSize):
                         board[row][col] = random.choice(possibilities)
-                else:
                     
 
 
@@ -362,7 +445,7 @@ while True:
     if not mouse_mode:
         if click[0]:
             mouseX,mouseY = mousePos()
-            if initialCoord == None:-
+            if initialCoord == None:
                 initialCoord = (mouseX,mouseY)
             dragdrop(initialCoord,mouseX,mouseY)
         else:
@@ -373,11 +456,18 @@ while True:
 
 
 
-            
+    # COPY #       
     draw_grid()
     drawGui()
     if smallGlider:
         drawSmallGlider(mouseX,mouseY)
     message=('Generation: '+str(generation))
     drawText(message)
+    if rules:
+        screen.fill(WHITE)
+        draw_rules_page_one()
+    if rules_2:
+        screen.fill(WHITE)
+        draw_rules_page_two()
     pygame.display.update()
+    ########
